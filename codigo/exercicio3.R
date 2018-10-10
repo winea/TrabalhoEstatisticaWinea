dd031f36828f057a3065881d95a5df5689f419a5
winea <winy2000@gmail.com>
2018-10-09 23:36:20 -0300

    tirado acento

--- codigo/exercicio3.R
+++ codigo/exercicio3.R
@@ -11,22 +11,22 @@ library(readxl)
 dados <- read_excel("dados/exercicio3.xlsx")
 View(dados)
 
-mediana <- median(dados$'Número de filhos')
+mediana <- median(dados$'Numero de filhos')
 mediana
 #moda
 getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
-moda <- getmode(dados$'Número de filhos')
+moda <- getmode(dados$'Numero de filhos')
 moda
 
-plot(dados$`Número de filhos`, xlab="Quantidade total", ylab="Número de filhos", main="Exercicio 3 - Mediana", col="red", las=1, type = "l")
+plot(dados$`Numero de filhos`, xlab="Quantidade total", ylab="Numero de filhos", main="Exercicio 3 - Mediana", col="red", las=1, type = "l")
 abline(mediana, 0, col="green")
 dev.copy(device = jpeg, file = "graficos/exercicio3_grafico1.jpeg", width = 600, height = 400, res = 100)
 dev.off()
 
-plot(dados$`Número de filhos`, xlab="Quantidade total", ylab="Número de filhos", main="Exercicio 3 - Moda", col="red", las=1, type = "l")
+plot(dados$`Numero de filhos`, xlab="Quantidade total", ylab="Numero de filhos", main="Exercicio 3 - Moda", col="red", las=1, type = "l")
 abline(moda, 0, col="deepskyblue")
 dev.copy(device = jpeg, file = "graficos/exercicio3_grafico2.jpeg", width = 600, height = 400, res = 100)
 dev.off()
